f724d644a2d1937ec241da5317664aee26d775e0
Gabriel <gabrielbeltrando@gmail.com>
2023-09-14 18:38:48 -0300

    Paquete inicial
    funciones basicas

---
+++ client/js/index.js
@@ -0,0 +1,40 @@
+const shopContent = document.getElementById("shopContent");
+const cart = []; //Este es un array vacio, nuestro carrito
+
+productos.forEach((product) => {
+    const content = document.createElement("div");
+    // content.className = "card";
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p>$ ${product.price}</p>
+    `;
+    shopContent.append(content);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    content.append(buyButton);
+
+    buyButton.addEventListener("click", () => {
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if (repeat) {
+            cart.map((prod) => {
+                if (prod.id === product.id){
+                    prod.quantity++;
+                    displayCartCounter();
+                }
+            });
+        } else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quantity: product.quantity,
+                img: product.img,
+            });
+            displayCartCounter();
+        }
+    })
+});
